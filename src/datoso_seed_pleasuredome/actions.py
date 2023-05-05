f390b585c98676c19defd0429389af45fd31bdd0
laromicas <laromicas@hotmail.com>
2023-05-04 22:49:04 -0500

    Removed unneeded dependencies, added actions

--- src/datoso_seed_pleasuredome/actions.py
+++ src/datoso_seed_pleasuredome/actions.py
@@ -1,7 +1,39 @@
-from datoso_seed_pleasuredome.dats import mame_dat_factory
+from datoso_seed_pleasuredome.dats import FruitMachinesDat, HomeBrewMameDat, mame_dat_factory
 
 actions = {
-    '{dat_origin}': [
+    '{dat_origin}/FruitMachines': [
+        {
+            'action': 'LoadDatFile',
+            '_class': FruitMachinesDat
+        },
+        {
+            'action': 'DeleteOld'
+        },
+        {
+            'action': 'Copy',
+            'folder': '{dat_destination}'
+        },
+        {
+            'action': 'SaveToDatabase'
+        }
+    ],
+    '{dat_origin}/HBMAME': [
+        {
+            'action': 'LoadDatFile',
+            '_class': HomeBrewMameDat
+        },
+        {
+            'action': 'DeleteOld'
+        },
+        {
+            'action': 'Copy',
+            'folder': '{dat_destination}'
+        },
+        {
+            'action': 'SaveToDatabase'
+        }
+    ],
+    '{dat_origin}/MAME': [
         {
             'action': 'LoadDatFile',
             '_factory': mame_dat_factory
@@ -16,7 +48,23 @@ actions = {
         {
             'action': 'SaveToDatabase'
         }
-    ]
+    ],
+    # '{dat_origin}/Reference': [
+    #     {
+    #         'action': 'LoadDatFile',
+    #         '_factory': mame_dat_factory
+    #     },
+    #     {
+    #         'action': 'DeleteOld'
+    #     },
+    #     {
+    #         'action': 'Copy',
+    #         'folder': '{dat_destination}'
+    #     },
+    #     {
+    #         'action': 'SaveToDatabase'
+    #     }
+    # ]
 }
 
 def get_actions():
