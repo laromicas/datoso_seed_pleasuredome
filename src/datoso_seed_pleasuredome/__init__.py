4da4fb09a96410ff71e3be73dd764e05780118ed
laromicas <laromicas@hotmail.com>
2024-05-04 16:14:14 -0500

    v1.0a0.dev0 advances

--- src/datoso_seed_pleasuredome/__init__.py
+++ src/datoso_seed_pleasuredome/__init__.py
@@ -1,6 +1,6 @@
 """__init__.py"""
 __all__ = ['__version__', '__author__', '__description__']
-__version__ = '1.0a0.dev0'
+__version__ = '1.0a0.dev1'
 __author__ = 'Lacides Miranda'
 __description__ = 'PleasureDome is a group that gathers dats for MAME, HBMAME and fruit machines.'
 __prefix__ = 'pleasuredome'
